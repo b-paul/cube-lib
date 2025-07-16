6e049c8abe00117761dfb2d9ff81b58516c22c55
bpaul <bpaul@bpaul.xyz>
2025-07-17 00:46:22 +1000

    Correct alg cancellation code (no tests yet)

--- src/moves/mod.rs
+++ src/moves/mod.rs
@@ -18,20 +18,27 @@ pub enum Cancellation<M: Move> {
     TwoMove(M, M),
 }
 
-/// Type of move that can be made to a puzzle.
-pub trait Move: Eq {
-    /// Invert a move.
-    ///
-    /// If `X` is a moves and `X^{-1}` is its inverse and `o` is composition, then
-    /// `X o X^{-1} = X^{-1} o X = e` where `e` is a move which does nothing.
+/// A move, for use in writing expressions or algorithms. It is intended that a term of this trait
+/// is a power of a symbol in some group presentation, satisfying law allowing simplification.
+///
+/// The relations moves satisfy should include an order for each term (e.g. R4 is the identity on a
+/// 3x3x3) and that some terms commute (e.g. R and L commute on a 3x3x3). Commutativity relations
+/// are encoded in the `commutes_with` method and order relations are encoded in the `cancel`
+/// method. These relations are all that are assumed for the general `MoveSequence::cancel`, so any
+/// additional relations will not be used for optimal cancellation.
+pub trait Move: Eq + Clone {
+    /// Take the inverse of a move. These inverses must satisfy the invertibility conditions of
+    /// a group, i.e. that `X X^{-1} = X^{-1} X = e` where `e` is the empty sequence.
     fn inverse(self) -> Self
     where
         Self: Sized;
 
-    /// Returns whether the two moves commute, i.e. can be swapped when adjacent.
+    /// Returns whether the two moves commute, i.e. can be swapped when adjacent. It is required
+    /// that this property is transitive.
     ///
-    /// If A and B are moves, then `A.commutes(B)` iff
+    /// If A and B are moves, then `A.commutes_with(B)` iff
     /// `A B = B A`
+    /// moreover, if `B.commutes_with(C)`, then it must be true that `A.commutes_with(C)`
     fn commutes_with(&self, b: &Self) -> bool;
 
     /// Return the cancellation of two moves.
@@ -69,40 +76,38 @@ impl<M: Move> MoveSequence<M> {
     }
 
     /// Cancel an alg completely, including rearrangement of commutative moves. This guarantees
-    /// optimal move count from cancellations.
+    /// optimal move count from cancellations (I THINK! TODO: PROVE THIS CLAIM!!).
     pub fn cancel(mut self) -> Self {
-        let mut cancellation = Vec::new();
-        let mut commuting_start = 0;
+        let mut cancellation: Vec<M> = Vec::new();
 
         for next_mv in self.0.drain(..) {
-            let i = cancellation.len();
-            if cancellation
-                .last()
-                .is_some_and(|m| next_mv.commutes_with(m))
-            {
-                cancellation.push(next_mv);
-                for j in (commuting_start..i).rev() {
-                    cancellation.swap(i - 1, j);
-                    let next_mv = cancellation.pop().unwrap();
-                    let mv = cancellation.pop().unwrap();
-                    match mv.cancel(next_mv) {
-                        Cancellation::NoMove => {
-                            commuting_start = commuting_start.min(i - 2);
-                            break;
-                        }
-                        Cancellation::OneMove(mv) => {
-                            cancellation.push(mv);
-                            break;
-                        }
-                        Cancellation::TwoMove(mv, next_mv) => {
-                            cancellation.push(mv);
-                            cancellation.push(next_mv);
-                        }
-                    }
+            // We work from the back of our fully reduced sub-expression, checking each move that
+            // we can commute with in sequence. We move the term we cancel with to the back, then
+            // cancel with it. Because our expression was previously fully reduced, moving this old
+            // term to the back will not create any new cancellations.
+            let mut cancelled = false;
+
+            for i in (0..cancellation.len()).rev() {
+                match cancellation[i].clone().cancel(next_mv.clone()) {
+                    Cancellation::NoMove => {
+                        cancellation.remove(i);
+                        cancelled = true;
+                    },
+                    Cancellation::OneMove(_) => {
+                        let new = cancellation.remove(i);
+                        cancellation.push(new);
+                        cancelled = true;
+                    },
+                    Cancellation::TwoMove(_, _) => {},
                 }
-            } else {
+
+                if !next_mv.commutes_with(&cancellation[i]) {
+                    break;
+                }
+            }
+
+            if !cancelled {
                 cancellation.push(next_mv);
-                commuting_start = i;
             }
         }
 
