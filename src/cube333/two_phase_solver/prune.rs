43a2fd10e6f9bd72ec90aba66fb094275312abb0
bpaul <bpaul@bpaul.xyz>
2025-12-15 12:49:57 +1000

    Conj table tests (need e slice fromcoord)

--- src/cube333/two_phase_solver/prune.rs
+++ src/cube333/two_phase_solver/prune.rs
@@ -2,10 +2,10 @@
 //!
 //! Choices of pruning tables are from cs0x7f's min2phase.
 
-use super::coords::SymCoordinate;
-use super::symmetry::Symmetry;
+use super::coords::{ESliceEdgeCoord, SymCoordinate};
+use super::symmetry::{HalfSymmetry, Symmetry};
 use crate::coord::{Coordinate, FromCoordinate};
-use crate::cube333::CubieCube;
+use crate::cube333::{CubieCube, coordcube::EOCoord};
 
 use std::marker::PhantomData;
 
@@ -15,18 +15,13 @@ use std::marker::PhantomData;
 //  look into alternative compression schemes
 
 /// A table storing results of conjugating raw coordinates by symmetries.
-pub struct SymConjTable<
-    S: Symmetry,
-    R: Coordinate<CubieCube>,
-    const SYMS: usize,
-> {
+pub struct SymConjTable<S: Symmetry, R: Coordinate<CubieCube>, const SYMS: usize> {
     table: Box<[[R; SYMS]]>,
     _phantom1: PhantomData<S>,
     _phantom2: PhantomData<R>,
 }
 
-impl<S: Symmetry, R: Coordinate<CubieCube>, const SYMS: usize>
-    SymConjTable<S, R, SYMS>
+impl<S: Symmetry, R: Coordinate<CubieCube>, const SYMS: usize> SymConjTable<S, R, SYMS>
 where
     CubieCube: FromCoordinate<R>,
 {
@@ -34,7 +29,7 @@ where
     pub fn generate() -> Self {
         let mut table: Box<[[R; SYMS]]> =
             vec![std::array::from_fn(|_| Default::default()); R::count()].into_boxed_slice();
-        
+
         for r1 in (0..R::count()).map(R::from_repr) {
             let mut c = CubieCube::SOLVED;
             c.set_coord(r1);
@@ -58,6 +53,9 @@ where
     }
 }
 
+type SliceConjTable = SymConjTable<HalfSymmetry, ESliceEdgeCoord, 8>;
+type EoConjTable = SymConjTable<HalfSymmetry, EOCoord, 8>;
+
 /// A pruning table indexed by the class of a symmetry coordinate and a raw coordinate.
 ///
 /// COUNT should be the number of symmetry coordinate classes times the number of raw coordinates
@@ -80,5 +78,46 @@ impl<S: SymCoordinate, R: Coordinate<CubieCube>, const COUNT: usize>
 
     pub fn query(&self, s: S, r: R) -> u8 {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::cube333::moves::Move333;
+    use crate::moves::MoveSequence;
+
+    use proptest::collection::vec;
+    use proptest::prelude::*;
+
+    #[test]
+    fn conj_generates() {
+        //SliceConjTable::generate();
+        EoConjTable::generate();
+    }
+
+    fn diagram_commutes<
+        S: Symmetry,
+        R: Coordinate<CubieCube> + std::fmt::Debug,
+        const COUNT: usize,
+    >(
+        table: &SymConjTable<S, R, COUNT>,
+        cube: CubieCube,
+    ) where
+        CubieCube: FromCoordinate<R>,
+    {
+        for s in S::get_all() {
+            let a = table.conjugate(R::from_puzzle(&cube), s);
+            let b = R::from_puzzle(&cube.clone().conjugate_symmetry(s));
+            assert_eq!(a, b);
+        }
+    }
+
+    #[test]
+    fn conj_commutes() {
+        let eo_conj_table = EoConjTable::generate();
+        proptest!(|(mvs in vec(any::<Move333>(), 0..20).prop_map(MoveSequence))| {
+            diagram_commutes(&eo_conj_table, CubieCube::SOLVED.make_moves(mvs.clone()));
+        });
     }
 }
